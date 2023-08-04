2f71251c994cb413b7b39df22f3c52fb3143006b
shoxrux <shoxruxnorqoziyev2004@gmail.com>
2023-08-04 10:49:40 +0500

    rows count

--- find_number_of_columns.py
+++ find_number_of_columns.py
@@ -1,3 +1,4 @@
+import csv
 def find_number_of_columns(data):
     """
     Find the number of columns in CSV.
@@ -6,6 +7,9 @@ def find_number_of_columns(data):
     Return:
         int: Number of columns.
     """
-    return
-
+    rows=data.split('\n')
+    bir_row=rows[0].split(',')
+    return len(bir_row)
+f=open('data.csv')
+print(find_number_of_columns(f.read()))
 # Read the csv file
